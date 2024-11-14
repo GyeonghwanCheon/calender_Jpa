e119fcf160076393a9decd708d2d76b77c344e90
myoma <qq8380qq@gmail.com>
2024-11-14 15:20:16 +0900

    init commit

---
+++ src/main/java/com/example/calenderjpa/CalenderJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.calenderjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalenderJpaApplication.class, args);
+    }
+
+}
